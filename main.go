4fac5383825c285b8bade17dec22b06120702ad4
kody.low <kodylow7@gmail.com>
2023-03-30 15:35:09 -0700

    formatting

--- main.go
+++ main.go
@@ -12,34 +12,37 @@ import (
 var infuraURL = "https://mainnet.infura.io/v3/360031b1b30f4b8b92b6a27850e11b8d"
 var ganacheURL = "http://localhost:8545"
 
+func gweiToEth(balance *big.Int) *big.Float {
+	return new(big.Float).Quo(new(big.Float).SetInt(balance), big.NewFloat(1000000000000000000))
+}
+
 func main() {
 
+	// Connect to a node
 	ethClient, err := ethclient.DialContext(context.Background(), ganacheURL)
 	if err != nil {
 		log.Fatalf("Failed to connect to the Ethereum client: %v", err)
 	}
 	defer ethClient.Close()
 
+	// Get the latest block
 	block, err := ethClient.BlockByNumber(context.Background(), nil)
 	if err != nil {
 		log.Fatalf("Failed to get the latest block: %v", err)
 	}
 	log.Printf("Latest block: %d", block.NumberU64())
 
+	// Get the balance of an address
 	addr := "0x0cd6f40fBceb4947749603cC069ed16D07FC548b"
 	address := common.HexToAddress(addr)
-
 	gweiBalance, err := ethClient.BalanceAt(context.Background(), address, nil)
 	if err != nil {
 		log.Fatalf("Failed to get balance: %v", err)
 	}
 	log.Printf("Balance Gwei: %d", gweiBalance)
 
+	// Convert to ETH
 	ethBalance := gweiToEth(gweiBalance)
 	log.Printf("Balance ETH: %f", ethBalance)
 
-}
-
-func gweiToEth(balance *big.Int) *big.Float {
-	return new(big.Float).Quo(new(big.Float).SetInt(balance), big.NewFloat(1000000000000000000))
 }
